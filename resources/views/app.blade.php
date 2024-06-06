e2311130f0f8a2d0d35b4beea5287f52361d7cbe
sagewwn <qscopra@outlook.com>
2024-06-06 11:42:08 +0200

    alles fix

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -4,13 +4,12 @@
     <meta charset="utf-8" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0" />
     @vite('resources/js/app.js')
-{{--    @inertiaHead--}}
+    @inertiaHead
+
 </head>
 <body>
+    @inertia
 
-    <div>
-        wehdsxcskjfhakjsfdhkjsafhvdzkl
-    </div>
-{{--@inertia--}}
+
 </body>
 </html>
--- routes/web.php
+++ routes/web.php
@@ -2,7 +2,12 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use Inertia\Inertia;
 
 Route::get('/', function () {
     return view('app');
+});
+
+Route::get('/show', function () {
+   return Inertia::render('Home/Show');
 });
